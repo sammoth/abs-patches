7647aeb0806646cfe42f46bfc978ca66cf997679
sammoth <sammoth.th@gmail.com>
2015-10-04 12:24:14 +0100

    first commit

---
+++ libfm/config.h
@@ -0,0 +1,109 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* Gettext package. */
+#define GETTEXT_PACKAGE "libfm"
+
+/* Define to 1 to enable custom actions */
+#define HAVE_ACTIONS 1
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the declaration of `environ', and to 0 if you
+   don't. */
+#define HAVE_DECL_ENVIRON 0
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `menu_cache_dir_list_children' function. */
+#define HAVE_MENU_CACHE_DIR_LIST_CHILDREN 1
+
+/* Have x-scheme/uri handler support */
+#define HAVE_SCHEME_HANDLER 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libfm"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://pcmanfm.sourceforge.net/"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libfm"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libfm 1.2.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libfm"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.2.3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable libexif */
+#undef USE_EXIF
+
+/* Enable UDisks support */
+#define USE_UDISKS 1
+
+/* Version number of package */
+#define VERSION "1.2.3"
+
+/* File offset bits */
+#define _FILE_OFFSET_BITS 64
+
+/* Whether to enable large file support */
+#define _LARGEFILE64_SOURCE 1
